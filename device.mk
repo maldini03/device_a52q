f55199b55552d688fc3100ae4c84bcd49df769c3
Nathan <Nathan@lunarixus.dev>
2021-08-31 14:53:33 +0000

    a52q: Remove some Gapps packages at build time

--- device.mk
+++ device.mk
@@ -31,3 +31,7 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
